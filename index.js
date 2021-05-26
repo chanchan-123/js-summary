34a78fbe9c54076bad9b26ceccf5fe8b3282d19c
chanchan-123 <1143909174@qq.com>
2021-05-26 19:57:05 +0800

    原型 原型链

--- index.js
+++
@@ -1,9 +0,0 @@
-function Person(name,age){
-  this.name = name;
-  this.age = age;
-  this.say = function(){
-    console.log(`我是${this.name}`)
-  }
-}
-let zhangsan = new Person('zhangsan',8)
-zhangsan.say()
--- knowledge/prototype/object1.js
+++ knowledge/prototype/object1.js
@@ -4,3 +4,41 @@
 // 普通对象 typeof object
 
 // 原型对象 prototype 共享
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.say = function() {
+    console.log(`我是${this.name}`);
+  };
+}
+let zhangsan = new Person('zhangsan', 8);
+zhangsan.say();
+
+/* 重点 */
+console.dir(zhangsan.__proto__ == Person.prototype);
+console.dir(Person.prototype.__proto__ == Object.prototype);
+
+console.dir(Person.__proto__ == Function.prototype);
+console.dir(Function.prototype.__proto__ == Object.prototype);
+console.dir(Object.prototype.__proto__ == null);
+
+/* 原型继承 */
+// 原型链的继承是原型对象的继承，不是改变构造函数的原型
+function Father() {}
+Father.prototype.view = function() {
+  console.log('Father View');
+};
+function Son() {}
+
+// 方法1
+Son.prototype.__proto__ = Father.prototype;
+
+// 方法2
+Son.prototype = Object.create(Father.prototype); // constructor会丢失
+console.log(Son.prototype.constructor); //Father
+
+Son.prototype.constructor = Son;
+console.log(Son.prototype.constructor); //Son
+
+let son1 = new Son();
+son1.view();
