5a7911e90df7cfeadf5a9c2c6828e59b0288f5be
chanchan-123 <1143909174@qq.com>
2021-06-01 11:24:45 +0800

    Proxy补充

--- knowledge/Proxy.js
+++ knowledge/Proxy.js
@@ -1,26 +1,24 @@
 let target = {
-  name:'zhangsan'
-}
+  name: 'zhangsan'
+};
 
-let pro = new Proxy(target,{
-  
-  get:function(target,key){
-    console.log('getter')
-    console.log(target,key)
-    return 35
+let pro = new Proxy(target, {
+  get: function(target, key) {
+    console.log('getter');
+    console.log(target, key);
+    return 35;
   },
-  set:function(target,key,value){
-    console.log('setter')
-    console.log(target,key,value)
-    return 25
-  },
-})
+  set: function(target, key, value) {
+    console.log('setter');
+    console.log(target, key, value);
+    return 25;
+  }
+});
 
-pro.name = 'lisi' // setter
+pro.name = 'lisi'; // setter
 
-pro.name // getter  35
-console.log(pro.name)
-
+pro.name; // getter  35
+console.log(pro.name);
 
 /**
  * 例子1 通过负索引获取数组的值
@@ -31,51 +29,77 @@ let arrProxy = new Proxy(arr, {
   get: function(target, key) {
     console.log(target, key, parseInt(key));
 
-    if (Number(key) != NaN && Number(key) < 0 && Number.isInteger(Number(key))) {
+    if (
+      Number(key) != NaN &&
+      Number(key) < 0 &&
+      Number.isInteger(Number(key))
+    ) {
       let index = parseInt(key) + target.length;
       return target[index];
-    }else{
-      return target[key]
+    } else {
+      return target[key];
     }
   }
 });
 
 console.log(arrProxy[1]);
 
-
 /**
  * 数据校验
  * 例子2 限制某些值的大小例如年龄
  */
 let target = {
-  name:'zhangsan'
-}
+  name: 'zhangsan'
+};
 
-let pro = new Proxy(target,{
-  
-  get:function(target,key){
-    console.log('getter')
-    console.log(target,key)
-    return target[key]
+let pro = new Proxy(target, {
+  get: function(target, key) {
+    console.log('getter');
+    console.log(target, key);
+    return target[key];
   },
-  set:function(target,key,value){
-    console.log('setter')
-    console.log(target,key,value)
-    if( key === 'age' && (value > 100 || value < 0) ){
+  set: function(target, key, value) {
+    console.log('setter');
+    console.log(target, key, value);
+    if (key === 'age' && (value > 100 || value < 0)) {
       console.log('年龄不符合实际');
-      target[key] = 100
-    }else{
+      target[key] = 100;
+    } else {
       target[key] = value;
     }
-  },
-})
+  }
+});
 
-pro.age = -5 // setter
+pro.age = -5; // setter
 
-console.log(pro.age)
-
+console.log(pro.age);
 
 /**
  * 关联属性
  * 例子3 通过邮政编码相关联
  */
+let targetCity = {
+  '001': '北京',
+  '002': '上海',
+  '003': '广州'
+};
+
+let pro = new Proxy(
+  {},
+  {
+    set: function(target, key, value) {
+      if (key === 'code') {
+        target[key] = targetCity[value];
+      }
+      return true;
+    },
+    // 如果有写getter的话必须要return,否则无法返回
+    get: function(target, key) {
+      console.log(target);
+      return target[key];
+    }
+  }
+);
+
+pro.code = '001'; // setter
+console.log(pro.code); // 北京
